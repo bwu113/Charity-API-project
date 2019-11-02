8363deedf5670aeb5c85c457c1c483a53dd652ea
bwu113 <55897484+bwu113@users.noreply.github.com>
2019-11-01 20:24:18 -0400

    Add files via upload

---
+++ Charity/forms.py
@@ -0,0 +1,17 @@
+from Charity import main
+import requests
+import sys, json
+from flask_wtf import FlaskForm
+from wtforms import SelectField
+
+# def locate_charity():
+
+api_key = main.read_from_file("JSON/API_Info.json")
+api_id = main.read_from_file("JSON/API_Info.json")
+api_key = api_key['key']
+api_id = api_id['id']
+url = "https://api.data.charitynavigator.org/v2/Organizations?app_id=" + api_id + "key=" + api_key
+request_json = requests.get(url).json()
+
+main.save_to_file(request_json, "JSON/charity_api.html")
+#Charity_api = main.read_from_file("JSON/charity_api.json")
---
+++ Charity/routes.py
@@ -0,0 +1,6 @@
+#from Charity import app, forms
+#from flask import request, render_template
+
+#@app.route('/', methods=['GET', 'POST'])
+#def search():
+ #   searchForm =
