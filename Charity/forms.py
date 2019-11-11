24e5f00e313e6ddf0f8060a44a1475c55151fd10
bwu113 <55897484+bwu113@users.noreply.github.com>
2019-11-10 22:16:46 -0500

    Add files via upload

--- Charity/forms.py
+++ Charity/forms.py
@@ -8,10 +8,13 @@ from wtforms import SelectField
 
 api_key = main.read_from_file("JSON/API_Info.json")
 api_id = main.read_from_file("JSON/API_Info.json")
-api_key = api_key['key']
-api_id = api_id['id']
-url = "https://api.data.charitynavigator.org/v2/Organizations?app_id=" + api_id + "key=" + api_key
+url = "https://api.data.charitynavigator.org/v2/Organizations?app_id=" + api_id['id'] + "&app_key=" + api_key['key']
 request_json = requests.get(url).json()
 
-main.save_to_file(request_json, "JSON/charity_api.html")
-#Charity_api = main.read_from_file("JSON/charity_api.json")
+main.save_to_file(request_json, "JSON/charity_api.json")
+Charity_api = main.read_from_file("JSON/charity_api.json")
+
+
+address
+charity_name
+rating
